7f83b2a62a1a5c4680e191240cbfed1dda7c4846
Thiago Ferraz <165716234+Dev-ferraz@users.noreply.github.com>
2024-04-11 21:04:33 -0300

    Add files via upload

---
+++ Primos.java
@@ -0,0 +1,40 @@
+package AAula1teste;
+import java.util.Scanner;
+
+public class Primos {
+
+	public static void main(String[] args) {
+
+
+		Scanner s = new Scanner(System.in);
+
+		boolean continuar = true;
+		
+		do {
+		    int numeroPrimo;
+			int numeDivisores = 0;
+			System.out.print("DIGITE UM NÚMERO: ");
+			numeroPrimo = s.nextInt();
+			
+			
+			for(int i =1; i <=numeroPrimo; i++) {
+				if(numeroPrimo % i ==0) {
+					numeDivisores ++;
+				}
+				
+			}
+			
+			if( numeDivisores  == 2) {
+				System.out.println("O número e Primo");
+				
+			}else {
+				System.out.println("O número não e Primo");
+			}
+
+			
+		}while(continuar);
+		
+	
+	}
+
+}
